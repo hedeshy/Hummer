b6ae5811fd68c854627047136f4de1233e20b87d
Raphael Menges <unrealer2@yahoo.de>
2020-07-15 22:49:10 +0200

    Now classy.

--- hum-recognition/recorder.py
+++ hum-recognition/recorder.py
@@ -1,55 +1,53 @@
 print('Recorder')
 
-# Intead of global variables, make class: https://stackoverflow.com/questions/32705518/using-pyaudio-callback-methods-in-a-user-defined-class
+# Overview about parameters in pyaudio: https://stackoverflow.com/questions/35970282/what-are-chunks-samples-and-frames-when-using-pyaudio
 
 import pyaudio
 import wave
 import time
 import keyboard
 
-CHANNELS: int = 2
-RATE: int = 44100
-FORMAT: int = pyaudio.paInt16
-WAVE_OUTPUT_FILENAME: str = 'file.wav'
+class Recorder:
 
-recorded_frames: bytes = bytes()
-stop_callback: bool = False
+	def _record_callback(self, in_data: bytes, frame_count: int, time_info: dict, status: int) -> (bytes, int):
+		self._recorded_frames = self._recorded_frames + in_data # TODO: this is super inefficient
+		callback_flag: int = pyaudio.paContinue
+		if self._stop_callback:
+			callback_flag = pyaudio.paComplete
+		return (None, callback_flag)
 
-p: pyaudio.PyAudio = pyaudio.PyAudio()
+	def __init__(self):
+		self._channels: int = 2
+		self._rate: int = 44100
+		self._format: int = pyaudio.paInt16
+		self._wav_filename: str = 'file.wav'
+		self._pa: pyaudio.PyAudio = pyaudio.PyAudio()
+		self._recorded_frames: bytes = bytes()
+		self._stop_callback: bool = False
 
-def callback(in_data: bytes, frame_count: int, time_info: dict, status: int) -> (bytes, int):
-	global recorded_frames
-	recorded_frames = recorded_frames + in_data
-
-	global stop_callback
-	if stop_callback:
-		callback_flag = pyaudio.paComplete
-	else:
-		callback_flag = pyaudio.paContinue
-
-	return (None, callback_flag)
-
-stream = p.open(format=FORMAT,
-				channels=CHANNELS,
-				rate=RATE,
+		stream = self._pa.open(format=self._format,
+				channels=self._channels,
+				rate=self._rate,
 				input=True,
-				stream_callback=callback)
+				stream_callback=self._record_callback)
 
-stream.start_stream()
+		stream.start_stream()
 
-while stream.is_active():
-	if keyboard.is_pressed('q'):
-		stop_callback = True
-	time.sleep(0.1)
+		while stream.is_active():
+			if keyboard.is_pressed('q'):
+				self._stop_callback = True
+			time.sleep(0.05)
 
-stream.stop_stream()
-stream.close()
+		stream.stop_stream()
+		stream.close()
 
-waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-waveFile.setnchannels(CHANNELS)
-waveFile.setsampwidth(p.get_sample_size(FORMAT))
-waveFile.setframerate(RATE)
-waveFile.writeframes(recorded_frames)
-waveFile.close()
+		with wave.open(self._wav_filename, 'wb') as w:
+			w.setnchannels(self._channels)
+			w.setsampwidth(self._pa.get_sample_size(self._format))
+			w.setframerate(self._rate)
+			w.writeframes(self._recorded_frames)
+			w.close()
 
-p.terminate()
+		self._pa.terminate()
+
+recorder: Recorder = Recorder()
