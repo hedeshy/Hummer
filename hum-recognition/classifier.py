a19345610b16eeae9e7a1b6fc0c0b2f86fbadc35
Raphael Menges <unrealer2@yahoo.de>
2020-07-18 00:29:09 +0200

    Initial classifier works.

--- hum-recognition/classifier.py
+++ hum-recognition/classifier.py
@@ -4,46 +4,99 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 from enum import Enum
+from typing import List
+from typing import Tuple
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+
+SEGMENT_WIDTH_S: float = 0.5
+SEGMENT_STEP_S: float = 0.1
+RATIO_OF_HUM: float = 0.5 # at least 50% of segment must contain humming to be labeled 'True'
+
+def compute_overlap(a, b):
+	return max(0, min(a[1], b[1]) - max(a[0], b[0]))
 
 # TODO: go over more than one file
 # Inspiration: https://www.kdnuggets.com/2020/02/audio-data-analysis-deep-learning-python-part-1.html
 
-# Import metadata
-meta = {}
-with open('data/raphael.json') as f:
-	meta = json.load(f)
-
-print(meta)
-
 # Test by import, make mono, export
 x, sr = librosa.load('data/raphael.wav', sr=None) # no resampling
 x_mono: np.ndarray = librosa.to_mono(x) # length of x_mono / sampling rate = duration in s
+length_s: float = x_mono.shape[0] / sr # length of clip in seconds
 librosa.output.write_wav('output.wav', x_mono, sr, norm=False)
 
 print(x_mono.shape)
 
 # Plot wave
-'''
-plt.figure(figsize=(14, 5))
-librosa.display.waveplot(x_mono, sr=sr)
-plt.show()
-'''
+# plt.figure(figsize=(14, 5))
+# librosa.display.waveplot(x_mono, sr=sr)
+# plt.show()
 
-# Split into pieces of certain length
-length_s = x_mono.shape[0] / sr # length of clip in seconds
-width_s = 1.0 # segment width
-step_s = 0.1 # segment stepping
-sample_count = width_s * sr
+# Import metadata
+meta: dict = {}
+with open('data/raphael.json') as f:
+	meta = json.load(f)
+
+print(meta['hums'])
+
+# Collect hums into starts and ends
+hum_starts_ms: List[int] = []
+hum_ends_ms: List[int] = []
+for ts, state in meta['hums']:
+	if state == 'start':
+		hum_starts_ms.append(ts)
+	else:
+		hum_ends_ms.append(ts)
+hum_starts_ms.sort()
+hum_ends_ms.sort()
+
+# Go over hums and make pairs of start and end (to be checked by later segments for intersection)
+hums_ms: List[Tuple[int,int]] = []
+if hum_starts_ms and hum_ends_ms:
+
+	# Check if there is a end before the first start (humming was going on at start of recording)
+	if hum_ends_ms[0] < hum_starts_ms[0]:
+		hum_starts_ms.insert(0,0)
+
+	for start_ms in hum_starts_ms:
+		success: bool = False
+		for end_ms in hum_ends_ms:
+			if end_ms > start_ms:
+				success = True
+				hums_ms.append((start_ms,end_ms))
+				break
+		if not success:
+			hums_ms.append(start_ms, int(length_s*1000))
 
 # Go over mono data and split into segments
-segments = []
+data: List[List[float]] = []
+target: List[int] = [] # right now: 0 or 1
 pos_s = 0.0
-while pos_s + width_s <= length_s:
-	start_idx = int(pos_s * sr)
-	end_idx = int((pos_s + width_s) * sr) - 1
-	y = x_mono[start_idx:end_idx]
-	pos_s += step_s
-	print(str(start_idx) + ', ' + str(end_idx))
+while pos_s + SEGMENT_WIDTH_S <= length_s:
+	
+	# Get audio segment
+	start_idx: int = int(pos_s * sr)
+	end_idx: int = int((pos_s + SEGMENT_WIDTH_S) * sr) - 1
+	y: np.array = x_mono[start_idx:end_idx]
+
+	# Compute label
+	label: int = 0 # no humming
+	start_ms: int = int(1000 * (start_idx / sr))
+	end_ms: int = int(1000 * (end_idx / sr))
+	overlap: int = 0
+	for hum_ms in hums_ms:
+		overlap += compute_overlap((start_ms, end_ms),hum_ms)
+	if float(overlap) / (SEGMENT_WIDTH_S*1000) >= RATIO_OF_HUM:
+		label = 1 # humming
+	target.append(label)
+
+	# Prepare next iteration
+	pos_s += SEGMENT_STEP_S
+	# print(str(start_idx) + ', ' + str(end_idx) + ': ' + str(label))
+	print('.', end='', flush=True)
 
 	# Compute features
 	rmse = librosa.feature.rms(y=y)[0]
@@ -64,44 +117,35 @@ while pos_s + width_s <= length_s:
 	seg.append(np.mean(zcr))
 	for e in mfcc: # 20 features
 		seg.append(np.mean(e))
+	data.append(seg)
+print()
 
-	# Store feature vector and label
-	segments.append((seg, True))
-	print(len(seg))
+# Convert dataset to numpy arrays
+data: np.array = np.array(data)
+target: np.array = np.array(target)
 
-	# TODO: check in meta data about humming or no humming is happening at this segment
-	# three states: neutral, negative, positive about humming
+# Split training and test data
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3, random_state=42)
+sm = SMOTE(random_state=42, sampling_strategy='not majority')
+X_train, y_train = sm.fit_resample(X_train, y_train)
 
-
-# TODO
-# 1. Split into segments
-# 2. Compute features on each segment and store corresponding label
-# 3. Train random forest or ANN
-
-# TODO: why is the first layer taking 256 values? There are only 26 features
-'''
-data = pd.read_csv('dataset.csv')
-data.head()# Dropping unneccesary columns
-data = data.drop(['filename'],axis=1)#Encoding the Labels
-genre_list = data.iloc[:, -1]
-encoder = LabelEncoder()
-y = encoder.fit_transform(genre_list)#Scaling the Feature columns
+# Scale data
 scaler = StandardScaler()
-X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))#Dividing data into training and Testing set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
-model = Sequential()
-model.add(layers.Dense(256, activation='relu', input_shape=(X_train.shape[1],)))
-model.add(layers.Dense(128, activation='relu'))
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+# Random Forest
+forest = RandomForestClassifier(
+	random_state=42,
+	n_estimators=25,
+	class_weight=None, # 'balanced', 'balanced_subsample'
+	criterion='entropy', # 'gini'
+	max_depth=None,
+	min_samples_split=2,
+	min_samples_leaf=1)
+forest.fit(X_train, y_train)
 
-
-classifier = model.fit(X_train,
-                    y_train,
-                    epochs=100,
-                    batch_size=128)
-
-'''
+# Predict on test set and print report
+y_pred = forest.predict(X_test).astype(int)
+print(classification_report(y_test, y_pred, target_names=['no humming', 'humming']))
